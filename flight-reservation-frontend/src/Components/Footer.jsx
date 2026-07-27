61e79f0356c9c8810f644d107c3d9a198d9445cd
Aniket Siraskar <aniketsiraskar9@gmail.com>
2026-07-28 00:30:00 +0530

    Revise Footer component with new details
    Updated footer content with new year and developer name.

--- flight-reservation-frontend/src/Components/Footer.jsx
+++ flight-reservation-frontend/src/Components/Footer.jsx
@@ -1,14 +1,12 @@
-export default function () {
+export default function Footer() {
   return (
-    <>
-      <footer>
-        <div className="container">
-          <p>
-            &copy; 2024 Flight Reservation System. All rights reserved. Designed
-            and developed by Cloudblitz
-          </p>
-        </div>
-      </footer>
-    </>
+    <footer>
+      <div className="container">
+        <p>
+          &copy; 2026 VanRaj Flight Reservation System. All rights reserved.
+          Designed and developed by Aniket Siraskar
+        </p>
+      </div>
+    </footer>
   );
 }
