c175038d1ac318b9d19c9d45c30cdfe7605cd821
Aniket Siraskar <aniketsiraskar9@gmail.com>
2026-07-27 16:28:27 +0530

    Add files via upload

---
+++ flight-reservation-infra-terraform-main/flight-reservation-infra-terraform-main/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "eu-north-1"
+}
+
+module "rds" {
+    source = "./modules/rds"
+}
+
+module "eks" {
+    source = "./modules/eks"
+    project = "cbz"
+    desired_nodes = 2
+    max_nodes  = 2
+    min_nodes  = 2
+    node_instance_type = "c7i-flex.large"
+}
+
+module "s3" {
+    source = "./modules/s3"
+}
