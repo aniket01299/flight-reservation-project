dcfdf05c59fdc5a22cb12d5c7522a84bae433aaf
Aniket Siraskar <aniketsiraskar9@gmail.com>
2026-07-27 22:20:49 +0530

    Add files via upload

---
+++ flight-reservation-frontend/frontend-app.groovy
@@ -0,0 +1,27 @@
+pipeline{
+    agent any
+    stages{
+        stage('Code-Pull'){
+            steps{
+                git branch: 'main', url: 'https://github.com/ayushhkamble/flight-reservation-frontend.git'    
+            }
+        }
+        stage('Code-Build'){
+            steps{
+                sh '''
+                    npm install
+                    npm run build
+                '''
+            }
+        }
+        stage('Deploy'){
+            steps{
+                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                    sh '''
+                       aws s3 sync dist/ s3://cbz-frontend-project-bucket-flight-reservation-app/ 
+                    '''
+                } 
+            }
+        }
+    }
+}
