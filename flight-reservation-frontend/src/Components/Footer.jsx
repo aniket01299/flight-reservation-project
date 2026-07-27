abcbd6e32c2d67ee59fdd6848e569908e488a674
Aniket Siraskar <aniketsiraskar9@gmail.com>
2026-07-28 00:36:08 +0530

    Update Footer.jsx

--- flight-reservation-frontend/src/Components/Footer.jsx
+++ flight-reservation-frontend/src/Components/Footer.jsx
@@ -1,12 +1,14 @@
-export default function Footer() {
+export default function () {
   return (
-    <footer>
-      <div className="container">
-        <p>
-          &copy; 2026 VanRaj Flight Reservation System. All rights reserved.
-          Designed and developed by Aniket Siraskar
-        </p>
-      </div>
-    </footer>
+    <>
+      <footer>
+        <div className="container">
+          <p>
+            &copy; 2024 Flight Reservation System. All rights reserved. Designed
+            and developed by Cloudblitz
+          </p>
+        </div>
+      </footer>
+    </>
   );
 }
