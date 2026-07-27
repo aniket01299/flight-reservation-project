ad0ea15b04fec8960f896c001073a823f2c79a96
Aniket Siraskar <aniketsiraskar9@gmail.com>
2026-07-27 22:15:58 +0530

    Delete flight-reservation-frontend directory

--- flight-reservation-frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-console.log("API URL:", import.meta.env.VITE_API_URL);
-
-const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
-});
-
-export default api;
