a75b0dad4a665010694afd4ad9f707da99e26f1d
Aniket Siraskar <aniketsiraskar9@gmail.com>
2026-07-30 11:40:03 +0530

    Refactor S3 bucket configuration for VanRaj project

--- flight-reservation-infra-terraform-main/modules/s3/main.tf
+++ flight-reservation-infra-terraform-main/modules/s3/main.tf
@@ -1,48 +1,54 @@
-# Create an S3 bucket
-resource "aws_s3_bucket" "cbz_bucket" {
-  bucket = "cbz-frontend-project-bucket-flight-reservation-app" # Replace with a globally unique bucket name
 
-  # Enable static website hosting
+# Create an S3 bucket
+resource "aws_s3_bucket" "vanraj_bucket" {
+  bucket = "vanraj-flight-reservation"
+
+  # Enable Static Website Hosting
   website {
     index_document = "index.html"
     error_document = "error.html"
   }
 
   tags = {
-    Name        = "StaticWebsiteBucket"
-    env = "dev"
+    Name = "VanRajFlightReservation"
+    env  = "dev"
   }
 }
 
 # Disable Block Public Access
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.cbz_bucket.bucket
+resource "aws_s3_bucket_public_access_block" "vanraj_public_access" {
+  bucket = aws_s3_bucket.vanraj_bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
-# Set the bucket policy to allow public read access (use cautiously)
-resource "aws_s3_bucket_policy" "static_website_policy" {
-  bucket = aws_s3_bucket.cbz_bucket.id
+
+# Bucket Policy for Public Read Access
+resource "aws_s3_bucket_policy" "vanraj_bucket_policy" {
+  bucket = aws_s3_bucket.vanraj_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
+        Sid       = "PublicReadGetObject"
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.cbz_bucket.arn}/*"
+        Resource  = "${aws_s3_bucket.vanraj_bucket.arn}/*"
       }
     ]
   })
-  depends_on = [aws_s3_bucket_public_access_block.example]
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.vanraj_public_access
+  ]
 }
 
-# Output the bucket's website endpoint
+# Output Website Endpoint
 output "website_endpoint" {
-  value       = aws_s3_bucket.cbz_bucket.website_endpoint
-  description = "The URL to access the static website"
+  description = "Static Website URL"
+  value       = aws_s3_bucket.vanraj_bucket.website_endpoint
 }
